9bb17325cfe943c6103f12e41d87ab98274e1fe6
bsandswebdev <bsandswebdev@gmail.com>
2015-08-09 20:23:34 -0400

    initial upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Gulp
+<p>Work in progress. Will continue to add to this file. As of this initial upload everything is working 100% as intended.</p>
---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+//Gulp
+var gulp = require('gulp');
+
+//Plugins
+var less = require('gulp-less-sourcemap'); //npm install gulp-less gulp-sourcemaps must also be installed
+var rename = require('gulp-rename');
+var minifyCss = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var path = require('path');
+var browserSync = require('browser-sync');
+var autoprefixer = require('gulp-autoprefixer');
+
+//IMAGEMIN
+gulp.task('images', function () {
+    return gulp.src('images/original/*')
+        .pipe(imagemin({
+        	optimizationLevel: 3,
+        	interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('images/optimized'));
+});
+
+//LESS + SOURCEMAPS
+gulp.task('less', function () {
+  gulp.src('less/theme.less')
+    .pipe(less({
+        sourceMap: {
+            sourceMapRootpath: 'less' // Optional absolute or relative path to your LESS files 
+        }
+    }))
+    .pipe(gulp.dest('css'));
+});
+
+//BROWSER SYNC
+gulp.task('browser-sync', function() {
+    browserSync.init(['css/theme.css'],['./*.html'] {        // files to inject
+        proxy: "localhost:8888"
+    });
+});
+
+//MINIFY-CSS 
+gulp.task('minify-css', function() {
+  return gulp.src('css/theme.css')
+    .pipe(minifyCss())
+    .pipe(rename('theme.min.css'))
+    .pipe(gulp.dest('css'));
+});
+
+//AUTOPREFIXER
+gulp.task('autoprefixer', function () {
+    return gulp.src('css/theme.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: true
+        }))
+        .pipe(gulp.dest('css'));
+});
+
+//Watch Files For Changes
+gulp.task('watch', function() {
+    
+    gulp.watch('less/*', ['less']);
+    gulp.watch('css/theme.css.map', ['less']);
+    gulp.watch('css', ['minify-css']);
+    gulp.watch('css/theme.css', ['autoprefixer']);
+    gulp.watch('images/original', ['images']);
+});
+
+// Default Task
+gulp.task('default', ['browser-sync', 'less', 'watch' ]);
+
