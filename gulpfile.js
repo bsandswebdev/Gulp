77b2244fcc9f09b6024cba55675d5f884b1b41df
Brian Sands <bsandswebdev@gmail.com>
2015-10-11 17:59:46 -0400

    reformat to code structure hierarchy. put minify-css task directly into sass task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,43 @@
 var gulp = require('gulp');
 
 //PLUGINS
+var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
 var minifyCss = require('gulp-minify-css');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
-var path = require('path');
-var browserSync = require('browser-sync');
-var autoprefixer = require('gulp-autoprefixer');
 
-//VARIABLES
-//var input = './scss/**/*.scss';  /*inputs all .scss files*/
-var input = './scss/theme.scss';
+//BROWSER SYNC
+gulp.task('browser-sync', function() {
+    browserSync.init(['css/theme.css', 'index.html'], {      		// Files to inject
+        /*server: {
+            baseDir: "./"
+        }*/
+        proxy: "localhost:8888"
+    });
+});
+
+//SAAS + SOURCEMAPS + AUTOPREFIXER + MINIFY CSS
+var input = './scss/theme.scss'; //var input = './scss/**/*.scss';  //Option to input all .scss files instead of import theme.scss
 var output = './css';
-var sassOptions = {
-	errLogToConsole: true,
-  	outputStyle: 'expanded'
-};
-var autoPrefixerOptions = {
-	browsers: ['last 2 versions'],
-    cascade: true
-}
+var sassOptions = {errLogToConsole: true, outputStyle: 'expanded'};
+var autoPrefixerOptions = {browsers: ['last 2 versions'], cascade: true}
+
+gulp.task('sass', function () {
+  return gulp
+    .src(input)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))				//Sass options      
+    .pipe(sourcemaps.write())										//Sourcemaps							    
+    .pipe(autoprefixer(autoPrefixerOptions))						//Autoprefixer			   
+    .pipe(gulp.dest(output))										//Output theme.css
+	.pipe(minifyCss())												//Minify CSS 										
+    .pipe(rename('theme.min.css'))									//Rename to .min file						
+    .pipe(gulp.dest('css'));										//Output minified CSS file
+});
 
 //IMAGEMIN
 gulp.task('images', function () {
@@ -38,41 +53,11 @@ gulp.task('images', function () {
         .pipe(gulp.dest('images/optimized'));
 });
 
-//SAAS + SOURCEMAPS + AUTOPREFIXER
-gulp.task('sass', function () {
-  return gulp
-    .src(input)
-    .pipe(sourcemaps.init())
-    .pipe(sass(sassOptions).on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(autoprefixer(autoPrefixerOptions))
-    .pipe(gulp.dest(output));
-});
-
-//BROWSER SYNC
-gulp.task('browser-sync', function() {
-    browserSync.init(['css/theme.css', 'index.html'], {        // files to inject
-        /*server: {
-            baseDir: "./"
-        }*/
-        proxy: "localhost:8888"
-    });
-});
-
-//MINIFY-CSS 
-gulp.task('minify-css', function() {
-  return gulp.src('css/theme.css')
-    .pipe(minifyCss())
-    .pipe(rename('theme.min.css'))
-    .pipe(gulp.dest('css'));
-});
-
 //Watch Files For Changes
 gulp.task('watch', function() {
     gulp.watch('images/original', ['images']);
     gulp.watch('scss/*', ['sass']);
     gulp.watch('css/theme.css.map', ['sass']);
-    gulp.watch('css/theme.css', ['minify-css']);
 });
 
 // Default Task
